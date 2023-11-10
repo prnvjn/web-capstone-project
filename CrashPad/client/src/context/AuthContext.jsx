97e464fda3fe4d7918fdbb6abdd9a340f8a88509
M Fazeel <mujaddadfazeel@gmail.com>
2023-11-10 00:52:51 -0600

    Created AuthContext.jsx

---
+++ CrashPad/client/src/context/AuthContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState(false);
+
+    const login = () => {
+        setAuth(true);
+    };
+
+    const logout = () => {
+        setAuth(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ auth, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
