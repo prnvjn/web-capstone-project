26755ab1ba091e8b766d9860fadc786f2379b066
M Fazeel <mujaddadfazeel@gmail.com>
2023-11-10 00:52:38 -0600

    Modified Navbar.jsx

--- CrashPad/client/src/components/Navbar.jsx
+++ CrashPad/client/src/components/Navbar.jsx
@@ -4,21 +4,70 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import { useAuth } from '../context/AuthContext'; // Adjust the path as needed
+import { useNavigate } from 'react-router-dom';
+
+import Menu from '@mui/material/Menu';
 
 const Navbar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const { auth, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        logout();
+        navigate('/');
+        setAnchorEl(null);
+    };
+
     return (
-        <AppBar position="static"  style={{ background: '#2E3B55' }}>
+        <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     CrashPad
                 </Typography>
-                <Button color="inherit" component={Link} to="/">
-                    Home
-                </Button>
-                <Button color="inherit" component={Link} to="/create-profile">
-                    Create Profile
-                </Button>
-                {/* Add more navigation links here */}
+                {auth ? (
+                    <div>
+                        <IconButton
+                            size="large"
+                            onClick={handleMenu}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                        </Menu>
+                    </div>
+                ) : (
+                    <div>
+                        <Button color="inherit" component={Link} to="/login">Login</Button>
+                        <Button color="inherit" component={Link} to="/create-profile">Sign Up</Button>
+                    </div>
+                )}
             </Toolbar>
         </AppBar>
     );
