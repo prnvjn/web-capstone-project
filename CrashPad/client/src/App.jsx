2832d564fe3576c0c5bce116fa19168a3fd2df1d
M Fazeel <mujaddadfazeel@gmail.com>
2023-11-10 00:52:17 -0600

    modified app.jsx

--- CrashPad/client/src/App.jsx
+++ CrashPad/client/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext'; // Adjust the path as needed
 import Navbar from './components/Navbar';
 import AppRoutes from './AppRoutes';
 
 function App() {
-  return (
-      <Router>
-          <Navbar />
-          <AppRoutes />
-      </Router>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Navbar />
+                <AppRoutes />
+            </Router>
+        </AuthProvider>
+    );
 }
-
 
 export default App;
