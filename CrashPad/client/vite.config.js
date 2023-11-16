6297120924e25fc7136f5c7637b807db0013df0b
M Fazeel <mujaddadfazeel@gmail.com>
2023-11-15 19:52:49 -0600

    modified for the server to handle requests

--- CrashPad/client/vite.config.js
+++ CrashPad/client/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      'picocss': path.resolve(__dirname, '../node_modules/@picocss/pico/css')
+    }
+  },
+  server: {
+    port: 3000, // or any other port you prefer
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173'
+      }
+    }
+  }
 })
