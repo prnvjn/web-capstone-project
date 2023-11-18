fdaeb073537cabbd33fdfb57a065d63b94fd321f
M Fazeel <mujaddadfazeel@gmail.com>
2023-11-17 21:52:38 -0600

    modified viewlistings

--- CrashPad/client/src/pages/ViewListings.jsx
+++ CrashPad/client/src/pages/ViewListings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ListingView } from "../components/ListingView";
 import { getCustomListings } from "../services/CustomListingsAPI";
 import { FormControlLabel, Checkbox } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const ViewListings = () => {
     const [data, setApartmentsData] = useState([]);
@@ -88,8 +89,34 @@ const ViewListings = () => {
                     />
                 </div>
             )}
-            
-            {filteredData.map((apt) => <ListingView key={apt.id} data={apt} />)}
+            {filteredData.map((data) => (
+      <div key={data.id} className="flex gap-8 m-10 bg-slate-100  max-w-4xl shadow-sm rounded-lg">
+       <div >
+        <img src={data.image} className="w-48 rounded-l-lg" />
+      </div>
+
+      <div className="p-6   flex-1" >
+        <h1 className=" text-xl mb-2 font-bold"> {data.name}</h1>
+        <h2 className="text-slate-200 text-sm mb-2 font-normal">{data.description}</h2>
+         <div className="grid grid-cols-2 gap 2 text-sm font-medium mb-2">
+            <div>{data.bedrooms} Bedrooms</div>
+             <div>{data.bathrooms} Bathrooms</div>
+         </div>
+         <div className="flex justify-between items-center">
+            <span className="font-bold mb-2">${data.price}</span>
+            <span>
+             <Link to={`/listings/${data.id}`}>
+             <button className="bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 hover:bg-blue-500 hover:text-white">
+               Click me
+                </button>
+              </Link>
+
+            </span>
+          </div>
+       </div>
+     </div>
+   ))}
+          
         </div>
     );
 };
